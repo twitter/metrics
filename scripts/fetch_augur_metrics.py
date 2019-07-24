12b9371ea52e7ed674ce7962acc48c77db5615a4
Jennifer Yen <jyen@twitter.com>
2019-07-24 16:41:08 -0700

    update API_ENDPOINT to newtwitter.augurlabs.io

--- scripts/fetch_augur_metrics.py
+++ scripts/fetch_augur_metrics.py
@@ -5,17 +5,20 @@ https://github.com/osshealth/augur
 Progress: https://github.com/twitter/metrics/issues/2
 """
 
+import datetime
 import json
 import os
-import datetime
+
 import requests
 
-API_ENDPOINT = "http://twitter.augurlabs.io/api/unstable"
+
+API_ENDPOINT = "http://newtwitter.augurlabs.io/api/unstable"
 PATH_TO_METRICS_DATA = "_data"
 PATH_TO_METADATA = "_metadata"
 DATESTAMP = datetime.datetime.now().date().isoformat()
 
 print("LOG: Assuming the current path to be the root of the metrics repository.")
+# tracked projects
 with open(os.path.join(PATH_TO_METADATA, "projects_tracked.json")) as f:
     PROJECTS_TRACKED = json.load(f)
 
@@ -31,6 +34,7 @@ once the API becomes reliable.
 
 Update _metadata/augur/bus_factor.json
 """
+
 BUS_FACTOR = {}
 bus_factor_json_file = f"{PATH_TO_METADATA}/augur/bus_factor.json"
 if os.path.exists(bus_factor_json_file):
@@ -54,60 +58,3 @@ for org in PROJECTS_TRACKED['projects']:
 
 with open(bus_factor_json_file, "w+") as f:
     json.dump(BUS_FACTOR, f)
-
-
-"""
-Timeseries of new watchers
-
-API: /:owner/:repo/timeseries/watchers
-
-This API endpoint gives a timeseries of new watchers in a date range.
-Not to be confused with actual number of watchers.
-Ref https://github.com/OSSHealth/augur/issues/152
-"""
-"""
-Old code leaved here for later reference
-
-New idea: use the endpoint to get aggregate new_watchers per year or something else
-"""
-
-# path_to_metrics_dir = f"{PATH_TO_METADATA}/augur/new_watchers.json"
-# for org in PROJECTS_TRACKED['projects']:
-#     for repo in PROJECTS_TRACKED['projects'][org]:
-#         dir_name = f"{path_to_metrics_dir}/{org}/{repo}"
-#         file_name = f"{dir_name}/data.json"
-#         os.makedirs(dir_name, exist_ok=True)
-
-#         print(f"Sending request to {API_ENDPOINT}/{org}/{repo}/timeseries/watchers")
-#         r = requests.get(f"{API_ENDPOINT}/{org}/{repo}/timeseries/watchers")
-#         try:
-#             if r.ok:
-#                 print("OK!")
-#                 data = r.json()[:-1] # Last metric is not valid
-
-#                 # Manipulation
-#                 new_watchers_timeseries = []
-
-#                 for i in range(1, len(data)):
-#                     old = data[i]
-#                     new = data[i - 1]
-#                     new_date = datetime.datetime.strptime(new["date"].split("T")[0], "%Y-%m-%d")
-#                     old_date = datetime.datetime.strptime(old["date"].split("T")[0], "%Y-%m-%d")
-
-#                     diff_days = (new_date - old_date).days
-#                     number = new["watchers"]
-
-#                     count_per_week = round(number/diff_days * 7, 2)
-#                     count_per_month = round(number/diff_days * 30, 2)
-
-#                     new_watchers_timeseries.append({"date": new_date.strftime("%Y-%m-%d"),
-#                                                     "count_per_week": count_per_week,
-#                                                     "count_per_month": count_per_month})
-#                 with open(file_name, "w+") as f:
-#                     json.dump(new_watchers_timeseries, f)
-#             else:
-#                 print(f"Error! Response code {r.status_code}")
-#                 print(r.content.decode("utf-8"))
-#         except Exception as e:
-#             raise(e)
-#             print(f"Error: Something went wrong with /:owner/:repo/timeseries/watchers - {e}")
